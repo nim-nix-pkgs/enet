f82368e81b607d9de324c0baa76f64b2d8487266
hugosenari <hugosenari@gmail.com>
2022-03-12 02:03:01 -0300

    chore: re index enet

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper for ENet UDP networking library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."enet-master".url = "path:./master";
+  inputs."enet-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."enet-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
