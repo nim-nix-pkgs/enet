2ed30bb5c8e06fbea4dffb18d418681b3cfb202a
hugosenari <hugosenari@gmail.com>
2022-03-27 18:15:17 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-enet-master.ref   = "refs/heads/master";
   inputs.src-enet-master.owner = "fowlmouth";
   inputs.src-enet-master.repo  = "nimrod-enet";
+  inputs.src-enet-master.dir   = "";
   inputs.src-enet-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
